abef8b6b8f152a2f130c922373433654e0cbc5ee
Chotipon113 <dxnishxzy14hz@gmail.com>
2025-06-18 15:25:40 +0000

    play_with_arrays++

---
+++ cell05/ex02/play_with_arrays++.py
@@ -0,0 +1,10 @@
+"""Test"""
+def main ():
+    """Test"""
+original_list = [2, 8, 9, 48, 8, 22, -12, 2]
+new_list = []
+for number in original_list:
+    if number > 5:
+        new_list.append(number + 2)
+print(new_list)
+main()
